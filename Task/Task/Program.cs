8af50235c49a8e102d4a242eb8f0517633e552d1
Ali <tu8cvbxnx@code.edu.az>
2022-03-05 07:14:31 +0400

    Loops

---
+++ Task/Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-ci Task
+            //string[] words = { "loops", "words", "english", "operators", "hello" };
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+
+            //    if (words[i].Length > 5)
+            //        Console.WriteLine(words[i]);
+            //}
+            #endregion
+
+
+            #region 2-ci Task
+            //Console.WriteLine("Enter a number: ");
+
+            //int sum = 0;
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //while (n > 0)
+            //{
+            //    sum += n % 10;
+            //    n /= 10;
+            //}
+
+            //Console.WriteLine(sum);
+            #endregion
+        }
+    }
+}
